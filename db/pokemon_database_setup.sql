c95763997468fbfb748fdd62ea59f675ed52b8a3
Morgan Lamb <morgan.lamb@hotmail.com>
2025-02-23 18:07:17 +0000

    Add pokemon_database_setup file that creates pokemon database and 5 tables

---
+++ db/pokemon_database_setup.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS pokemon_db;
+CREATE DATABASE pokemon_db;
+
+\c pokemon_db;
+
+CREATE TABLE IF NOT EXISTS pokemon
+(
+    pokemon_id INT PRIMARY KEY,
+    pokemon_name VARCHAR(20),
+    base_experience INT,
+    type_1 VARCHAR(10),
+    type_2 VARCHAR(10),
+    hp_stat INT,
+    attack_stat INT,
+    defense_stat INT,
+    special_attack_stat INT,
+    special_defense_stat INT,
+    speed INT
+);
+
+CREATE TABLE IF NOT EXISTS pokemon_abilities
+(
+    pokemon_id INT,
+    ability_id INT
+);
+
+ALTER TABLE pokemon_abilities
+    ADD FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS abilities
+(
+    ability_id INT PRIMARY KEY,
+    ability_name VARCHAR(20),
+    ability_description VARCHAR(100)
+);
+
+ALTER TABLE pokemon_abilities
+    ADD FOREIGN KEY (ability_id) REFERENCES abilities(ability_id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS pokemon_moves
+(
+    pokemon_id INT,
+    move_id INT,
+    level_learnt INT,
+    learn_method VARCHAR(50)
+);
+
+ALTER TABLE pokemon_moves
+    ADD FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS moves
+(
+    move_id INT PRIMARY KEY,
+    move_name VARCHAR(20),
+    move_description VARCHAR(100),
+    move_type VARCHAR(10),
+    powerpoints INT,
+    accuracy INT,
+    power INT, 
+    category VARCHAR(10)
+);
+
+ALTER TABLE moves
+    ADD FOREIGN KEY (move_id) REFERENCES move(move_id) ON DELETE CASCADE;
+
+SELECT * FROM pokemon;
